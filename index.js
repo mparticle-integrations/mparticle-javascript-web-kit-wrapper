85b5b6ff31feebc82fd73f8aa860f9e663bed163
Robert Ing <ring@mparticle.com>
2018-11-14 14:05:31 -0500

    Fix typo logPagView to logPageView

--- index.js
+++ index.js
@@ -13,12 +13,12 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-var CommerceHandler = require('../../integration-builder/commerce-handler');
-var EventHandler = require('../../integration-builder/event-handler');
-var IdentityHandler = require('../../integration-builder/identity-handler');
-var Initialization = require('../../integration-builder/initialization');
-var SessionHandler = require('../../integration-builder/session-handler');
-var UserAttributeHandler = require('../../integration-builder/user-attribute-handler');
+var CommerceHandler = require('../../../integration-builder/commerce-handler');
+var EventHandler = require('../../../integration-builder/event-handler');
+var IdentityHandler = require('../../../integration-builder/identity-handler');
+var Initialization = require('../../../integration-builder/initialization');
+var SessionHandler = require('../../../integration-builder/session-handler');
+var UserAttributeHandler = require('../../../integration-builder/user-attribute-handler');
 
 (function (window) {
     var name = Initialization.name,
@@ -118,7 +118,7 @@ var UserAttributeHandler = require('../../integration-builder/user-attribute-han
 
         function logPageView(event) {
             try {
-                EventHandler.logPagView(event);
+                EventHandler.logPageView(event);
                 return true;
             } catch (e) {
                 return {error: 'Error logging page view on forwarder ' + name + '; ' + e};
